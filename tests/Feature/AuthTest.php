61ca69364a9c2ebdc0b593cbb061e1653ad1e9ae
Tom <tomfbrady1@gmail.com>
2024-06-28 10:56:30 +0100

    Add a feature test for auth and remove uneeded web route

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+
+class AuthTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@test.com',
+            'password' => 'password',
+            'api_token' => '',
+        ]);
+    }
+
+    public function testApiTokenGeneratedAndReturnedOnSuccessfulAuthentication(): void
+    {
+        $response = $this->postJson('/api/authenticate', [
+            'email' => 'test@test.com',
+            'password' => 'password',
+        ]);
+
+        $user = User::where('email', 'test@test.com')->first();
+
+        $response->assertStatus(200);
+        $response->assertJson(['apiToken' => $user->api_token]);
+    }
+
+    public function testApiTokenNotGeneratedAndReturnedOnUnsuccessfulAuthenticationAttempt(): void
+    {
+        $response = $this->postJson('/api/authenticate', [
+            'email' => 'test@test.com',
+            'password' => 'wrongPassword',
+        ]);
+
+        $user = User::where('email', 'test@test.com')->first();
+
+        $response->assertStatus(401);
+        $this->assertEquals($user->api_token, '');
+    }
+}
